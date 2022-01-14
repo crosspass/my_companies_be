483d34ed59785b85a4d24c195cf2d9bbea8d5075
iiithinking <wuyingchao1@gmail.com>
2022-01-14 15:59:27 +0800

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -x
+
+bin_file="my-companies-be"
+new_bin_file="${bin_file}-new"
+work_home="~/my_companies_be/"
+
+GOOS=linux GOARCH=amd64 go build -o ./${new_bin_file}
+
+scp ${new_bin_file} rails:${work_home}
+
+ssh -T rails << 'EOF'
+  systemctl --user stop vnote.club.service
+  mv ~/my_companies_be/my-companies-be-new ~/my_companies_be/my-companies-be
+  systemctl --user start vnote.club.service
+EOF
